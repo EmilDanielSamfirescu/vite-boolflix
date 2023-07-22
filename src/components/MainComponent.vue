5aae65d8b6193db90f0edb3ffd81c81a3bedf90a
EmilDanielSamfirescu <emil.danielsam@gmail.com>
2023-07-22 15:12:55 +0200

    milestone 2

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,36 +1,41 @@
 <script >
+
 import {store} from '../store.js';
+import SingleElement from './SingleElement.vue';
+
 export default {
     name: "MainComponent",
-data (){
-    return {
-        store
-    }
-},
-methods: {
-    getFlag(language) {
-        if(language == 'it'){
-            return 'https://flagicons.lipis.dev/flags/4x3/it.svg';
-        } else if(language == 'en') {
-            return 'https://flagicons.lipis.dev/flags/4x3/gb.svg';
-        }else if(language == 'de') {
-            return 'https://flagicons.lipis.dev/flags/4x3/de.svg';
-        }else if(language == 'es') {
-            return 'https://flagicons.lipis.dev/flags/4x3/es.svg';
-        }else if(language == 'fr') {
-            return 'https://flagicons.lipis.dev/flags/4x3/fr.svg';
-        }else if(language == 'ja') {
-            return 'https://flagicons.lipis.dev/flags/4x3/jp.svg';
-        }else if(language == 'pt') {
-            return 'https://flagicons.lipis.dev/flags/4x3/pt.svg';
-        } else {
-            return '';
-            return 'https://flagicons.lipis.dev/flags/4x3/xx.svg';
+    components: {
+        SingleElement,
+    },
+    data (){
+        return {
+            store
+        }
+    },
+    methods: {
+        getFlag(language) {
+            if(language == 'it'){
+                return 'https://flagicons.lipis.dev/flags/4x3/it.svg';
+            } else if(language == 'en') {
+                return 'https://flagicons.lipis.dev/flags/4x3/gb.svg';
+            }else if(language == 'de') {
+                return 'https://flagicons.lipis.dev/flags/4x3/de.svg';
+            }else if(language == 'es') {
+                return 'https://flagicons.lipis.dev/flags/4x3/es.svg';
+            }else if(language == 'fr') {
+                return 'https://flagicons.lipis.dev/flags/4x3/fr.svg';
+            }else if(language == 'ja') {
+                return 'https://flagicons.lipis.dev/flags/4x3/jp.svg';
+            }else if(language == 'pt') {
+                return 'https://flagicons.lipis.dev/flags/4x3/pt.svg';
+            } else {
+                return '';
+                return 'https://flagicons.lipis.dev/flags/4x3/xx.svg';
+            }
         }
 
     }
-
-}
 };
 </script>
 
@@ -41,24 +46,9 @@ methods: {
         </h2>
         <ul>
             <li v-for="(movie, i) in store.movies" :key="i">
-                <ol>
-                    <li>
-                        {{ movie.title }}
-                    </li>
 
-                    <li>
-                        {{ movie.original_title }}
-                    </li>
-
-                    <li>
-                        <img :src="getFlag(movie.original_language)" :alt="movie.original_language.toUpperCase()">
-                    </li>
-
-                    <li>
-                        {{ movie.vote_average }}
-                    </li>
-
-                </ol>
+                <SingleElement :elementData="movie"/>
+                
             </li>
         </ul>
 
@@ -67,24 +57,9 @@ methods: {
         </h2>
         <ul>
             <li v-for="(singleSeries, i) in store.series" :key="i">
-                <ol>
-                    <li>
-                        {{ singleSeries.name }}
-                    </li>
 
-                    <li>
-                        {{ singleSeries.original_name }}
-                    </li>
-
-                    <li>
-                        <img :src="getFlag(singleSeries.original_language)" :alt="singleSeries.original_language.toUpperCase()">
-                    </li>
-
-                    <li>
-                        {{ singleSeries.vote_average }}
-                    </li>
-
-                </ol>
+                <SingleElement :elementData="singleSeries"/>
+                
             </li>
         </ul>
     </main>
--- src/components/SingleElement.vue
+++ src/components/SingleElement.vue
@@ -1,21 +1,68 @@
 <script >
 export default {
     name: "SingleElement",
-data (){
-    return {
+    data (){
+        return {
 
+        }
+    },
+    props:{
+        elementData: {
+            type: Object,
+            default: null
+        }
+    },
+    methods: {
+        getFlag(language) {
+            if(language == 'it'){
+                return 'https://flagicons.lipis.dev/flags/4x3/it.svg';
+            } else if(language == 'en') {
+                return 'https://flagicons.lipis.dev/flags/4x3/gb.svg';
+            }else if(language == 'de') {
+                return 'https://flagicons.lipis.dev/flags/4x3/de.svg';
+            }else if(language == 'es') {
+                return 'https://flagicons.lipis.dev/flags/4x3/es.svg';
+            }else if(language == 'fr') {
+                return 'https://flagicons.lipis.dev/flags/4x3/fr.svg';
+            }else if(language == 'ja') {
+                return 'https://flagicons.lipis.dev/flags/4x3/jp.svg';
+            }else if(language == 'pt') {
+                return 'https://flagicons.lipis.dev/flags/4x3/pt.svg';
+            } else {
+                return '';
+                return 'https://flagicons.lipis.dev/flags/4x3/xx.svg';
+            }
+        }
     }
-},
-methods: {
-
-}
 };
 </script>
 
 <template>
-    
+    <div>
+        <ol>
+            <li>
+                <!-- ?? è un operatore binario, funziona tipo l'OR -->
+                {{ elementData.title ?? elementData.name}}
+            </li>
+
+            <li>
+                {{ elementData.original_title ?? elementData.original_name}}
+            </li>
+
+            <li>
+                <img :src="getFlag(elementData.original_language)" :alt="elementData.original_language.toUpperCase()">
+            </li>
+
+            <li>
+                {{ elementData.vote_average }}
+            </li>
+
+        </ol>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+img {
+        width: 30px;
+    }
 </style>
